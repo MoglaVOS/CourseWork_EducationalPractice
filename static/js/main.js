b1cd4b2aca2079d8bc9445429e39fb6990647a24
darkground <blockads32@gmail.com>
2024-05-15 01:19:06 +0300

    feat: static files for calendar and dashboard

---
+++ static/js/main.js
@@ -0,0 +1,44 @@
+(function () {
+	"use strict";
+
+	var treeviewMenu = $('.app-menu');
+
+	$("[lang = 'ru']").addClass('active');
+
+	// Toggle Sidebar
+	$('[data-toggle="sidebar"]').click(function(event) {
+		event.preventDefault();
+		$('.app').toggleClass('sidenav-toggled');
+	});
+
+	// Activate sidebar treeview toggle
+	$("[data-toggle='treeview']").click(function(event) {
+		event.preventDefault();
+		if(!$(this).parent().hasClass('is-expanded')) {
+			treeviewMenu.find("[data-toggle='treeview']").parent().removeClass('is-expanded');
+		}
+		$(this).parent().toggleClass('is-expanded');
+	});
+
+	// Set initial active toggle
+	$("[data-toggle='treeview.'].is-expanded").parent().toggleClass('is-expanded');
+
+	//Activate bootstrip tooltips
+	$("[data-toggle='tooltip']").tooltip();
+
+	$.fn.datepicker.dates['ru'] = {
+		days: ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
+		daysShort: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+		daysMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+		months: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+		monthsShort: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+		today: "Сегодня",
+		clear: "Очистить",
+		format: "dd.mm.yyyy",
+		titleFormat: "MM yyyy", /* Leverages same syntax as 'format' */
+		weekStart: 1
+	};
+
+	//$("[dataformatas = 'date']").datepicker({dateFormat: 'yy-mm-dd'});
+
+})();
