74fcf92f843ec374d511c2d305fbd4b31eb7c15e
gutsy47 <bestcom5055@gmail.com>
2024-05-15 00:02:59 +0300

    Feat: Init apps

--- palendar/settings.py
+++ palendar/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'event',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'palendar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'event/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
