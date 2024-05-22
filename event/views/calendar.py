1196cf8b0fa4f4edb07a45dcb63af23455ce3339
gutsy47 <bestcom5055@gmail.com>
2024-05-23 01:56:48 +0300

    Feat: Calendar Tabs
    Updated View CalendarView to show other users' tabs. The tabs themselves have been added to calendar.html.
    NOT ADDED the ability to view/edit/create events in other people's calendars.
    
    Reviewed CalendarView:
     - New field owner
     - New field invites
     - New field is_owner_in_overflow

--- event/views/calendar.py
+++ event/views/calendar.py
@@ -1,12 +1,15 @@
 from django.shortcuts import redirect, render, get_object_or_404
-from django.views.generic import View, TemplateView
+from django.urls import reverse
+from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from datetime import datetime, timedelta
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 
 from event.forms import EventChangeForm, EventCreateForm
 from event.models import Event
 from event.models.notification import Notification
+from account.models.invite import Invite
+from account.models.user import User
 
 
 class CalendarView(LoginRequiredMixin, View):
@@ -17,14 +20,27 @@ class CalendarView(LoginRequiredMixin, View):
     form_class = EventCreateForm
 
     def get(self, request):
-        form = self.form_class()  # Create event form
-        events_today = Event.objects.get_events_by_date(user=request.user, date=datetime.today())  # Side panel
-        notifications = Notification.objects.get_all_notifications(user=request.user, time=timedelta(days=2))
-        event_list = []  # Calendar table
+        # Calendar owner. Inviter if provided else user
+        inviter_id = request.GET.get("inviter_id")
+        if inviter_id:
+            try:
+                owner = get_object_or_404(User, id=inviter_id)
+                if not Invite.objects.filter(invitee_email=request.user.email, inviter=owner).exists():
+                    new_request = request.GET.copy()
+                    new_request.pop("inviter_id")
+                    return redirect(reverse("event:calendar") + '?' + new_request.urlencode())
+            except Http404:
+                owner = request.user
+        else:
+            owner = request.user
 
-        # Fill calendar table
-        for event in Event.objects.get_all_events(user=request.user):
-            event_list.append({
+        form = self.form_class()  # Create event form
+        notifications = Notification.objects.get_all_notifications(user=request.user, time=timedelta(days=2))
+        invites = Invite.objects.get_invites_by_invitee(request.user).filter(status=1)  # Other`s calendar tabs
+        is_owner_in_overflow = True if owner in [x.inviter for x in invites[3:]] else False  # For dropdown menu
+        events_today = Event.objects.get_events_by_date(user=owner, date=datetime.today())  # Side panel`s events
+        event_list = [
+            {
                 "id": event.id,
                 "title": event.title,
                 "start": event.start_time.strftime("%Y-%m-%dT%H:%M:%S"),
@@ -33,14 +49,20 @@ class CalendarView(LoginRequiredMixin, View):
                 "type": event.type,
                 "priority": event.priority,
                 "description": event.description,
-            })
+            } for event in Event.objects.get_all_events(user=owner)
+        ]  # Main calendar data
 
         context = {
             "form": form,
+            "user": request.user,
+            "owner": owner,
+            "invites": invites,
+            "is_owner_in_overflow": is_owner_in_overflow,
             "events": event_list,
             "events_today": events_today,
             "notifications": notifications
         }
+
         return render(request, self.template_name, context)
 
     def post(self, request):
@@ -83,6 +105,7 @@ class EventChangeView(LoginRequiredMixin, View):
         context = {"form": form}
         return render(request, self.template_name, context)
 
+
 def notif_read(request):
     if request.method == 'POST':
         notif_id = request.POST.get('notif_id')
@@ -96,7 +119,8 @@ def notif_read(request):
         return JsonResponse({'message': 'Complete.'})
     else:
         return JsonResponse({'message': 'Error!'}, status=400)
-    
+
+
 def delete_event(request, event_id):
     event = get_object_or_404(Event, id=event_id)
 
