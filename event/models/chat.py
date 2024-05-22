b1dbf082c1af144ad7bb83d9acda6032f0095085
darkground <blockads32@gmail.com>
2024-05-23 01:09:22 +0300

    improve: chat model event foreign key

--- event/models/chat.py
+++ event/models/chat.py
@@ -1,23 +1,17 @@
 from django.db import models
 
 from account.models import User
+from event.models.event import Event
 
 
 class ChatMessageManager(models.Manager):
     """ Chat manager """
 
     @staticmethod
-    def get_all_messages():
-        return ChatMessage.objects.filter(is_deleted=False)
-
-    @staticmethod
-    def get_message_count():
-        return ChatMessage.objects.filter(is_deleted=False).count()
-
-    @staticmethod
-    def get_recent_messages():
-        return ChatMessage.objects.filter(is_deleted=False).order_by("start_time")[:10]
-
+    def get_all_messages(event: Event | None = None):
+        if event:
+            return ChatMessage.objects.filter(is_deleted=False, event=event)
+        return ChatMessage.objects.filter(is_deleted=False, event=None)
 
 class ChatMessage(models.Model):
     """ Chat message model """
@@ -25,6 +19,7 @@ class ChatMessage(models.Model):
     # Main Fields
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages")
     message = models.CharField(max_length=4000)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="messages", null=True, blank=True)
 
     # Static fields
     is_deleted = models.BooleanField(default=False)
